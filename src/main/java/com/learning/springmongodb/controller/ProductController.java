ec98ffb98d49c5ba65faf9fcb28e1320be94664c
= <vanudevi@gmail.com>
2020-07-10 12:30:18 +0400

    get property using config server

--- src/main/java/com/learning/springmongodb/controller/ProductController.java
+++ src/main/java/com/learning/springmongodb/controller/ProductController.java
@@ -2,21 +2,29 @@ package com.learning.springmongodb.controller;
 
 import com.learning.springmongodb.bean.Product;
 import com.learning.springmongodb.bean.Response;
+import com.learning.springmongodb.config.DownstreamProperties;
 import com.learning.springmongodb.daoImpl.ProductDaoImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
+@RefreshScope
 public class ProductController {
 
     @Autowired
     private ProductDaoImpl productImpl;
+
+    @Value("${info.precedence}")
+    private String precedenceValue;
+
+    @Autowired
+    private DownstreamProperties downstreamProperties;
 
     @PostMapping(path="/products")
     public ResponseEntity<Product> save(@RequestBody Product product) {
@@ -36,4 +44,16 @@ public class ProductController {
             @RequestParam(value="limit", defaultValue = "5", required = true) String limit){
         return productImpl.findByTextIndex(search, Integer.parseInt(start), Integer.parseInt(limit));
     }
+
+
+    @GetMapping(path="/propValues")
+    public String getPropertiesValue(){
+        return precedenceValue;
+    }
+
+    @GetMapping(path="/propBean")
+    public DownstreamProperties getDownstreamProperties(){
+        return downstreamProperties;
+    }
+
 }
